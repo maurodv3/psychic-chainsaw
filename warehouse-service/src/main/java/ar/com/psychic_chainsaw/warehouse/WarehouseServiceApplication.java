84a2597f451de2291c083503d4c436896d862798
Mauro Vidal <mauro.vidal@jivesoftware.com>
2019-01-06 13:46:46 -0300

    Add registry module. Rename warehouse app.

--- warehouse-service/src/main/java/ar/com/psychic_chainsaw/warehouse/WarehouseServiceApplication.java
+++ warehouse-service/src/main/java/ar/com/psychic_chainsaw/warehouse/WarehouseServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WarehouseApplication {
+public class WarehouseServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WarehouseApplication.class, args);
+        SpringApplication.run(WarehouseServiceApplication.class, args);
     }
 
 }
--- warehouse-service/src/test/java/ar/com/psychic_chainsaw/warehouse/WarehouseServiceApplicationTests.java
+++ warehouse-service/src/test/java/ar/com/psychic_chainsaw/warehouse/WarehouseServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WarehouseApplicationTests {
+public class WarehouseServiceApplicationTests {
 
     @Test
     public void contextLoads() {
